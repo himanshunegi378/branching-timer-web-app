5805ea6bc20333ac67bb28090302b3d771ece607
himanshunegi378 <davidxavier378@gmail.com>
2020-06-09 09:41:48 +0530

    add sound on timer finishing

--- src/component/countDownTimer/countDownTimer.jsx
+++ src/component/countDownTimer/countDownTimer.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import store from "../../store/store";
 import countDownClock from "../../core/countDownClock";
-import { saveTimer,timerFinished } from '../../slices/timerSlice';
+import { Howl } from 'howler';
+import alarmSound from './alarm.mp3'
+
+
+import { saveTimer, timerFinished } from '../../slices/timerSlice';
 function CountDownTimer() {
     const activeTimerId = useSelector(state => state.timer.activeTimer.id)
     const [currentTimerId, setCurrentTimerId] = useState('')
@@ -11,14 +15,20 @@ function CountDownTimer() {
     const onTick = ({ days, hours, minutes, seconds }) => {
     }
 
-    const onStop=()=>{
-        console.log('countdown finished')
-        dispatch(timerFinished({id:activeTimerId}))
+    const onStop = () => {
+        var sound = new Howl({
+            src: [alarmSound]
+        });
+        let id = sound.play();
+        setTimeout(() => {
+            sound.stop(id)
+        }, 3000);
+        dispatch(timerFinished({ id: activeTimerId }))
     }
     useEffect(() => {
         if (activeTimerId !== currentTimerId && activeTimerId) {
             let activeTimer = store.getState().timer.timers[activeTimerId]
-            countDownClock(onStop,onTick,{mins:activeTimer.mins,secs:activeTimer.secs})
+            countDownClock(onStop, onTick, { mins: activeTimer.mins, secs: activeTimer.secs })
             console.log(' play timer', activeTimerId, activeTimer)
         }
     }, [activeTimerId])
