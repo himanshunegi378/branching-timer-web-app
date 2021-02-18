7d01c923c5fce599b6725a97da9a729f49e01357
himanshunegi378 <davidxavier378@gmail.com>
2021-02-18 19:32:16 +0530

    Updates

---
+++ precache-manifest.d06b84a4d7655e5dbd3d6934fba7f830.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8dd13bbfdd0c7300d693140e3cd0388",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ada61ff888be916a8988",
+    "url": "/static/css/2.09383595.chunk.css"
+  },
+  {
+    "revision": "2659cd69873655a1a040",
+    "url": "/static/css/main.72cce3ef.chunk.css"
+  },
+  {
+    "revision": "ada61ff888be916a8988",
+    "url": "/static/js/2.aa1b51e8.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.aa1b51e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2659cd69873655a1a040",
+    "url": "/static/js/main.3d092ed8.chunk.js"
+  },
+  {
+    "revision": "665b9549de97061aeb46",
+    "url": "/static/js/runtime-main.015e0e1d.js"
+  }
+]);
